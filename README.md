85e1d28300c204943a05de2c5f86719955555cfb
GunPKate <gun@builk.com>
2022-10-19 10:05:14 +0700

    add read me

---
+++ README.md
@@ -0,0 +1,10 @@
+## How to use Hapi and Joi
+
+```
+npm i @hapi/hapi
+npm i joi
+```
+
+### joi is used for Schema and validation
+### Hapi conducts server and create router for RESTAPI
+### Hapi contains path (endpoint), method, and handler (input response and return result)
--- test.js
+++ test.js
@@ -1,28 +1,34 @@
-const hapi = require("@hapi/hapi");
+// const hapi = require("@hapi/hapi");
 
-const init = async () => {
-  const server = await hapi.server({
-    port: 3000,
-    host: "localhost",
-  });
+// const init = async () => {
+//   const server = await hapi.server({
+//     port: 3000,
+//     host: "localhost",
+//   });
 
-  server.route({
-    method: "get",
-    path: "/",
-    handler: (req, h) => {
-      return "Hello";
-    },
-  });
+//   server.route({
+//     method: "get",
+//     path: "/",
+//     handler: (req, h) => {
+//       return "Hello";
+//     },
+//   });
 
-  
+//   await server.start();
+//   console.log("Server running on %s", server.info.uri);
+// };
 
-  await server.start();
-  console.log("Server running on %s", server.info.uri);
-};
+// process.on("unhandledRejection", (err) => {
+//   console.log(err);
+//   process.exit(1);
+// });
 
-process.on("unhandledRejection", (err) => {
-  console.log(err);
-  process.exit(1);
-});
+// init();
 
-init();
+const b = setTimeout(() => {
+  console.log("B");
+}, 1000);
+
+setTimeout(() => {
+  console.log("A");
+}, 2000).then(b);
